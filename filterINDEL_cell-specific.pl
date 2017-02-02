5e8a6699affb6284a8f80de57abf6936220a125d
rvanboxtel <rvanboxtel@hpcs03.op.umcutrecht.nl>
2017-02-02 15:27:04 +0100

    releasing version 1.0.0 INDELfilter

--- filterINDEL_cell-specific.pl
+++ filterINDEL_cell-specific.pl
@@ -2,16 +2,65 @@
 use strict;
 use Getopt::Long qw(GetOptions);
 
+sub show_version{
+    warn <<END;
+    Version 1.0.0 (copyright R. van Boxtel 2017)
+END
+    exit;
+}
+
+sub usage{
+    warn <<END;
+
+    Usage:
+    Run by typing: perl filterINDEL_cell-specific.pl -i [input VCF] -s [column test sample] -c [column control sample] -out [output directory]
+
+    Required parameters:
+    -i | input VCF (including the PATH)
+    -s | column number, which contains the genotype field of the TEST sample in the VCF (0-based)
+    -c | column number, which contains the genotype field of the CONTROL sample in the VCF (0-based)
+    -out | output directory
+
+    Optional parameters:
+    -q | minimal GATK quality score for filtering (Default: 400)
+    -RD | minimal read depth at a variant position in both TEST and CONTROL sample to pass the filter (Default: 20)
+    -VAF | minimal variant allele frequency in the TEST sample for a variant to pass the filter (Default: 0.3)
+
+END
+    exit;
+}
+
+die usage() if @ARGV == 0;
+
 my $vcf;
 my $sample;
 my $control;
 my $out_dir;
+my $qual = 400; #optional parameter
+my $min_rd = 20;
+my $min_vaf = 0.3; #optional parameter
+my $help;
+my $version;
 GetOptions(
     'i=s' => \$vcf,
     's=s' => \$sample,
     'c=s' => \$control,
     'out=s' => \$out_dir,
-) or die "ERROR: parameters missing (-i <INPUT>, -s <SAMPLE_COLUMN>, -c <CONTROL_COLUMN> or -out <OUPUT DIRECTORY>)\n";
+    'q=s' => \$qual,
+    'RD=s' => \$min_rd,
+    'VAF=s' => \$min_vaf,
+    'help' => \$help,
+    'version' => \$version,
+);
+
+if ($help){
+    usage();
+}
+
+if ($version){
+    show_version();
+}
+
 
 #--get names of the samples
 my $sample_name = "Error";
@@ -46,7 +95,7 @@ while (my $line = <IN>){
 	next if ($data[0] eq "X" or $data[0] eq "Y" or $data[0] eq "MT"); #get autosomal genome;
 	next if ($data[2] =~ m/rs/ and $data[2] !~ m/COSM/); #remove SNP_ids w/o COSMIC id
 	next if ($data[4] =~ m/\,/);
-	next if ($data[5] < 100); #minimal quality requirement
+	next if ($data[5] < $qual); #minimal quality requirement
 	next if ($data[6] ne "PASS"); #only consider passed variants
 
 	my @info_control = split(":", $data[$control]); #get specs for control sample
@@ -62,7 +111,7 @@ while (my $line = <IN>){
 	    }
 	}
 	my $AA_control = $RD_control - $alleles_control[0];
-	next if ($RD_control < 20); #remove positions with less than 20 informative reads in ref sample
+	next if ($RD_control < $min_rd); #remove positions with less than 20 informative reads in ref sample
 	next if ($AA_control > 0); #remove positions with any evidence in ref sample
 
 	my @info_sample = split(":", $data[$sample]); #get specs for test sample
@@ -77,12 +126,12 @@ while (my $line = <IN>){
 	    $RD_sample = $RD_sample + $allele;
 	    }
 	}
-	next if ($RD_sample < 20); #remove positions with less than 20 informative reads in test sample
+	next if ($RD_sample < $min_rd); #remove positions with less than 20 informative reads in test sample
 	my $AA = $RD_sample - $alleles_sample[0];
 	my $VAF = $AA/$RD_sample;
 
 	next if ($AA == 0); #remove calls with no alternative reads, but a call in sample
-	next if ($VAF < 0.3); #remove samples with low VAF
+	next if ($VAF < $min_vaf); #remove samples with low VAF
 
 	print OUT $line, "\n";
     }
